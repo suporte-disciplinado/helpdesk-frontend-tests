71139c9c94876b7f040e5c9f6d1a7ef4755da95b
albertocesario-infnet <alberto.cesario@al.infnet.edu.br>
2025-04-08 22:14:14 -0300

    Initial Commit

---
+++ src/test/java/com/suportedisciplinado/frontendtests/KnowledgebasePageTest.java
@@ -0,0 +1,17 @@
+package com.suportedisciplinado.frontendtests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnowledgebasePageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/com/suportedisciplinado/frontendtests/UsersPageTest.java
@@ -0,0 +1,17 @@
+package com.suportedisciplinado.frontendtests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersPageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
